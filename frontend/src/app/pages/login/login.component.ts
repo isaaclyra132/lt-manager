17e631b7b615741ab629880377cc10fd9b85aa66
Isaac de Lyra <isaac.lyra.junior@gmail.com>
2023-09-08 01:35:08 -0300

    :sparkles: feat: Login page with forms and its validations

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,8 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  providers: [],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  submitted: boolean = false;
+  loading: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit() {
+    this.createFormLogin();
+  }
+
+  createFormLogin() {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  login() {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      // TODO - Chamar o service de autenticacao com as credenciais inseridas
+      this.showSuccessLoginToast();
+    } else {
+      this.showFailedLoginToast();
+    }
+  }
+
+  showFailedLoginToast() {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Credenciais inválidas',
+      detail: 'Preencha suas credenciais corretamente',
+      life: 3000,
+    });
+  }
+
+  showSuccessLoginToast() {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Login bem-sucedido',
+      detail: 'Bem-vindo ao LT.Manager',
+      life: 3000,
+    });
+    console.log('deu certo');
+  }
+}
